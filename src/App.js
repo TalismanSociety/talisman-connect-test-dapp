76887f45f8decce550ae805c20e9a5254cb2ab5b
Josh <ahimaz.joshua@gmail.com>
2022-04-04 13:18:13 +0800

    Refactoring and testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import logo from './logo.gif';
 import './App.css';
 
+import {
+  web3InWindow,
+  web3Accounts,
+  web3FromAddress,
+  web3FromSource,
+  isWeb3Injected,
+  web3EnablePromise,
+  web3AccountsSubscribe
+} from './talisman-connect/bundle.ts';
+
+import {  web3Enable as taliweb }  from './talisman-connect/bundle.ts';
+
+
+import {
+  web3Enable
+} from '@polkadot/extension-dapp'
+
+import { useEffect } from 'react';
+
 function App() {
+
+  const injectedExtensions = async () => {
+    await web3InWindow()
+    await taliweb('my cool dapp', 'talisman')
+    await web3Enable("cool daap")
+  }
+  // const selectedExtension = async () => 
+
+  useEffect(() => {
+    injectedExtensions();
+    // selectedExtension();
+  })
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Big Wonderful Testing Thingy <span className="Version-Number">1.0.0</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
